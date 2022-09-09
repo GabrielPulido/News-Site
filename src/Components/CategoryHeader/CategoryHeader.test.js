079eb36a1baf92cc51d27808e1e3343d385aa64c
GabrielPulido <GabrielPulido0056@gmail.com>
2022-09-09 11:41:36 -0700

    fixed bug in CategoryHeader tests

--- src/Components/CategoryHeader/CategoryHeader.test.js
+++ src/Components/CategoryHeader/CategoryHeader.test.js
@@ -3,13 +3,13 @@ import CategoryHeader from './CategoryHeader';
 import { categoriesList } from '../../utils';
 
 test('category header is rendered properly', () => {
-    render(<CategoryHeader {...categoriesList[0]} />);
+    render(<CategoryHeader category={categoriesList[0]} />);
     const categoryHeader = screen.getByRole('heading', { name: categoriesList[0].name })
     expect(categoryHeader).toBeInTheDocument();
 });
 
 test('image is rendered properly', () => {
-    render(<CategoryHeader {...categoriesList[0]} />);
+    render(<CategoryHeader category={categoriesList[0]} />);
     const img = screen.getByRole('img', { name: 'business_logo' })
     expect(img).toBeInTheDocument();
 });
